01227e9012aab03d740ea16f726b0887d930536d
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-03-26 14:41:11 +0700

    membuat logic perhitungan

---
+++ pertemuan_kelima.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+void inputData() {
+    cout << "Masukan bilangan pertama : " ;
+    cin >> bilangan1 ;
+    cout << "Masukan bilangan kedua : " ;
+    cin >> bilangan2;
+}
+
+int penjumlahan(int a, int b) {
+    return a + b;
+}
+int pengurangan(int a, int b) {
+    return a - b;
+}
+int perkalian(int a, int b) {
+    return a * b;
+}
+float pembagian(int a, int b) {
+    return a / b;
+}
+
+
+
+int main() {
+
+}
